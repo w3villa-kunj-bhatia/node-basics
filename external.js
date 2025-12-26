821d7a23981aba2fd5087d0a662a8d8a0942f6ae
Kunj Bhatia <kunj.bhatia@w3villa.com>
2025-12-26 11:51:55 +0530

    node_modules and code using internal/external libraries added

--- external.js
+++ external.js
@@ -1,6 +1,12 @@
-const factorial = require('./factorial');
-const greet = require('./greet');
+const { log } = require("console");
+const factorial = require("./factorial");
+const greet = require("./greet");
 
-console.log(greet('Kunj'));
-console.log(factorial(5));
+//this adds colors to the console output
+const chalk = require("chalk");
 
+console.log(chalk.blue(greet("Kunj")));
+console.log(chalk.greenBright(factorial(5)));
+
+console.log(factorial(12));
+console.log(greet("Crow"));
