dc7a2c1a5b8e60817b7a8a5e9d3d7f1b9728f433
Kunj Bhatia <kunj.bhatia@w3villa.com>
2025-12-30 18:24:29 +0530

    Refactor server request handling and logging; enhance response for signup and search routes

--- server/index.js
+++ server/index.js
@@ -1,29 +1,38 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") {
     return res.end();
   }
 
-  const log = `${Date.now()}: ${req.url} New request received\n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
+
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
 
   fs.appendFile("log.txt", log, (err) => {
-    if (err) {
-      res.end("Error writing to log");
-      return;
-    }
-
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
+        if (req.method == "GET") res.end("Home Page");
         res.end("Home Page");
         break;
       case "/about":
-        res.end("I am Kunj Bhatia");
+        const username = myUrl.query.username || "Guest";
+        res.end("Hi ${username}, Welcome to About Page");
         break;
-      case "/contact":
-        res.end("Contact us");
+      case "/search":
+        const search = myUrl.query.q || "";
+        res.end("Here are your results for: ${search}");
         break;
+      case "/signup":
+        if (req.method == "GET") res.end("This is a Signup Page");
+        else if (req.method == "POST") {
+          res.end("Form Submitted Successfully");
+          //DB Query
+          res.end("Success!");
+        }
       default:
         res.end("404 Page Not Found");
     }
