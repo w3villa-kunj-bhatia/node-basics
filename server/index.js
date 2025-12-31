3eea5f8dab04f5e2275a3758fbce3cc1f4c73fc1
Kunj Bhatia <kunj.bhatia@w3villa.com>
2025-12-31 10:40:19 +0530

    code refactoring

--- server/index.js
+++ server/index.js
@@ -6,30 +6,25 @@ const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") {
     return res.end();
   }
-
   const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
-
   const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
 
-  fs.appendFile("log.txt", log, (err) => {
+  fs.appendFile("log.txt", log, (err, data) => {
     switch (myUrl.pathname) {
       case "/":
-        if (req.method == "GET") res.end("Home Page");
-        res.end("Home Page");
+        if (req.method === "GET") res.end("Home Page");
         break;
       case "/about":
-        const username = myUrl.query.username || "Guest";
-        res.end("Hi ${username}, Welcome to About Page");
+        const username = myUrl.query.myname;
+        res.end(`Hi ${username}, Welcome to About Page`);
         break;
       case "/search":
-        const search = myUrl.query.q || "";
-        res.end("Here are your results for: ${search}");
+        const search = myUrl.query.search_query;
+        res.end("Here are your results for: " + search);
         break;
       case "/signup":
-        if (req.method == "GET") res.end("This is a Signup Page");
-        else if (req.method == "POST") {
-          res.end("Form Submitted Successfully");
+        if (req.method === "GET") res.end("This is a Signup Page");
+        else if (req.method === "POST") {
           //DB Query
           res.end("Success!");
         }
